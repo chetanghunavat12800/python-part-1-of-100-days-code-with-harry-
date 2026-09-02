9a7ffbbc261753dbb2ff9b065c848ab7c96dc7e6
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:30:21 +0530

    Sync project file: userinput.py

---
+++ userinput.py
@@ -0,0 +1,13 @@
+a=input("enter your name: ")
+print("My name is: ",a)
+
+# it takes input only integer values 
+x=int(input("enter num1: "))
+y=int(input("enter num2: "))
+print(x+y)
+
+# this take string as a input and typecast it but fails when you not give numerical string 
+p=input("enter fistNUM: ")
+q=input("enter secondNUM: ")
+print(int(p)+int(q))
+
