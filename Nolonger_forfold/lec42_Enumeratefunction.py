893859334ce61582cff3d9a6fab3c70bd2855fea
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:55 +0530

    Sync project file: Nolonger_forfold/lec42_Enumeratefunction.py

---
+++ Nolonger_forfold/lec42_Enumeratefunction.py
@@ -0,0 +1,8 @@
+#....what is enumerate function ..............
+list1=['chetan','abhay','lokendra','shivam']
+for i in enumerate(list1):
+  print(i) 
+
+print(type(enumerate(list1)))
+print(enumerate(list1))
+print(*enumerate(list1))
