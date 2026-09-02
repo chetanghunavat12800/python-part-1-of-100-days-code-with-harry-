acd437920e1ff73d79b241c93f0ff9aca3ca15df
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:28:59 +0530

    Sync project file: array.py

---
+++ array.py
@@ -0,0 +1,9 @@
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+numbers.remove(numbers[4])
+print(numbers)
+numbers.remove(3)
+print(numbers)
+
+#  there are some other types of functions like lists ,tuple    
+
