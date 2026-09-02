230d8dfbe59facf9bec72eb4066649115d35895f
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:30:16 +0530

    Sync project file: string.py

---
+++ string.py
@@ -0,0 +1,9 @@
+
+name="chetan meena"
+friend='rohan kumar'
+conversation="hello rohan, today is your \"BIRTHDAY\" "  # if you dont apply \"    \"  the python gets confused
+fun='okay i have bought a "gift" for you '  # no case of confusion 
+#print("i am ",name,"\n""my friend is ",friend,"\n",conversation,sep="")
+
+for i in name[2:8:1]:  # after a gap of 1 i=i+1 goes here 
+  print(i) 
