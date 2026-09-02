a7e7aa9ac0e278b2dc8f7f8d071e2146f5577d34
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:15 +0530

    Sync project file: datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+a = 5
+b = 4
+c = a + b
+d=True
+e=None
+
+f="ghee" # yeh ek str hai ....  
+h='k'    # single character bhi str hota hai ....
+s=complex(8,2)
+print(c)
+print(s)
+print("the type of s complex ",type(s));
+print(type(c),type(d),type(e),type(f),type(h)) 
+
