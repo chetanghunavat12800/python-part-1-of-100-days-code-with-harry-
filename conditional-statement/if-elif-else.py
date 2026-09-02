8abb6727f63383d6d9739819c9ddb9b6b0c09f26
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:13 +0530

    Sync project file: conditional-statement/if-elif-else.py

---
+++ conditional-statement/if-elif-else.py
@@ -0,0 +1,13 @@
+age=int(input("please enter your Age: "))
+print("your age is :",age)
+
+if (age>=18):
+  print("you are eligible to drive.")
+  print("Yes")
+else:
+  print("you are a minor. you are not eligible to drive. ")
+  print("NO")
+  print("ohhh shit") #indentation se bahar hai so loop iske upar tk hi chalegi....
+
+
+
