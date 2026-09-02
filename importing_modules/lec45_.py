3d8fe6286c50edd7a3d11273d7d6bdc72e095d53
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:39 +0530

    Sync project file: importing_modules/lec45_.py

---
+++ importing_modules/lec45_.py
@@ -0,0 +1,9 @@
+import mymodule as md # here i can also import my modules in the python program
+
+ans=md.fact(10)
+print(ans)
+# import math as m
+print(md.average([1,2,3,4,5,6,7,8,9,10])) 
+print(md.ceil(6),md.ceil(465.794),md.hypot(4,3))
+print(dir(md))
+
