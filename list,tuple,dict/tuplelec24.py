d9ca857c48610355babb6dbd708c96ba82dd93ac
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:46 +0530

    Sync project file: list,tuple,dict/tuplelec24.py

---
+++ list,tuple,dict/tuplelec24.py
@@ -0,0 +1,10 @@
+#.....tuples ......
+# tuples are  immutable they cannot be change ......
+tup=(1,3,5,6,7,"chetan meena","age=20",25)
+print(tup)
+# tup[0]=46   this gives error 
+print(tup[5],tup[3],tup[6],tup[0])
+
+
+# comprehension in tuples
+tup1=()
