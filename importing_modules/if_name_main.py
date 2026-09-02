b6f6dac4b16c2a702246a3fc79de2708e83c7c76
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:35 +0530

    Sync project file: importing_modules/if_name_main.py

---
+++ importing_modules/if_name_main.py
@@ -0,0 +1,9 @@
+def greeting():
+  print('''
+May your life always be blessed with good health, endless happiness, true friendships, and the courage to chase every dream that lives in your heart.
+  ''')
+
+print(__name__)
+if(__name__=='__main__'):
+  greeting()
+  
