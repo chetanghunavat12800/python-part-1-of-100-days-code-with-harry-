1cca1740ff13b75731059b47186b095a4b6b6fa4
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:21 +0530

    Sync project file: error-exception-HANDLE/lec36_exception-handle.py

---
+++ error-exception-HANDLE/lec36_exception-handle.py
@@ -0,0 +1,9 @@
+a=input("please enter the number : ")
+
+try:
+  for i in range(1,11):
+    print(f"{int(a)}X{i}={int(a)*i}")
+except Exception as e: 
+  print("hahahahah")
+
+print("some lines of code ")
