90dd97b1663b0a26c1a6a0a7a9b07e84cc103a68
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:30:12 +0530

    Sync project file: set-dictonary/set-lec31.py

---
+++ set-dictonary/set-lec31.py
@@ -0,0 +1,12 @@
+# set unordered data collectection with no duplicates 
+set1={100, 7, 45, 999, 2, 17}
+print(set1)
+print(sorted(set1))
+set2={'a','z','r','b','n','s','c','z','r'}
+print(set2)    
+print(sorted(set2))
+set3={}
+print(type(set3))
+
+set4=set()
+print(type(set4)) 
