87f9fcd9f90489aee5a4509cac07c274f257f976
chetanghunavat12800 <chetanghunavat12800@gmail.com>
2026-09-03 02:29:10 +0530

    Sync project file: basics.py

---
+++ basics.py
@@ -0,0 +1,20 @@
+for i in range(1,5):  # yaha pr start include hota hai and last did not included 
+    print(i,"hello")
+
+
+for i in range(5):  # yaha pr start 0 se hota hai  and last did not included 
+   print(i,"chetan")
+
+
+i = 0 
+while i<=5:
+  print(i,"meena")
+  i+=1
+
+
+for i in range(0,10,1):
+  print(i,"yeh loop bilkul cpp jesi hai like for(int i=0;i<10;i++")
+
+print("\n\n\n")
+
+
